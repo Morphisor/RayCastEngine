db98e99630644e51cae737af4f1c74035130b664
Morphisor <a.gallitri@gmail.com>
2017-07-31 14:40:07 +0200

    generic entity setup

---
+++ RayCast.Core/Enums/EntityStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayCast.Core.Enums
+{
+    public enum EntityStatus
+    {
+        Attacking,
+        Moving,
+        Idle,
+        Dead
+    }
+}
---
+++ RayCast.Core/Interfaces/IEntity.cs
@@ -0,0 +1,27 @@
+﻿using RayCast.Core.Components;
+using RayCast.Core.Enums;
+using RayCast.Core.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayCast.Core.Interfaces
+{
+    public interface IEntity
+    {
+        AnimatedSprite AnimatedSprite { get; set; }
+        PathFinding PathFinding { get; set; }
+        EntityStatus Status { get; set; }
+        int[] AttackFrames { get; set; }
+        int[] IdleFrames { get; set; }
+        int[] DeathFrames { get; set; }
+        int Ealth { get; set; }
+        int Damage { get; set; }
+
+        void UpdateEntity();
+        void Attack();
+        void TakeDamage();
+    }
+}
